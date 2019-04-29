139396b59750c5f30f85a2faf962bc2aa31bceb2
Joe B <joeberusch@gmail.com>
2019-04-29 21:41:29 +0000

    added send times

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time
+import math
+import random
 import schedule
 from twilio.rest import Client
 
@@ -10,14 +12,19 @@ client = Client(account_sid, auth_token)
 messages = ['What\'s next?', 'What\'s the worst that could happen']
 
 def send_death_msg():
+	print("Sending message...")
 	message = client.messages \
         .create(
-             body="Remember, you're going to die. Don't forget! " + messages[math.floor(random.random() * len(messages))],
+             body="Remember, you're going to die. Don't forget! " + messages[int(math.floor(random.random() * len(messages)))],
              from_='+12165022834',
              to='+12165448688'
          )
 
-schedule.every().minutes.do(send_death_msg)
+# schedule.every().minutes.do(send_death_msg)
+times = ["08:30", "14:00", "20:00", "23:00", "16:21"]
+for t in times:
+    print("scheduling message to send at time: " + str(t))
+    schedule.every().day.at(t).do(send_death_msg)
 
 while True:
 	schedule.run_pending()
